8e2b60de5f950f9d9051b54ae68fea8b6c453045
taekg <94892850+taekg@users.noreply.github.com>
2024-04-25 17:34:52 +0900

    [Gold II] Title: 보석 도둑, Time: 548 ms, Memory: 30708 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cstring>
+#include<queue>
+using namespace std;
+
+struct Cubic
+{
+	long long weight;
+	long long value;
+
+	bool operator<(Cubic Right) const {
+		if (weight > Right.weight)return true;
+		if (weight < Right.weight)return false;
+		return false;
+	}
+};
+
+struct lanecb
+{
+	long long weight;
+	long long value;
+	bool operator<(lanecb right) const {
+		if (value < right.value)return true;
+		if (value > right.value)return false;
+		return false;
+	}
+};
+
+priority_queue<Cubic>Cv;
+priority_queue<lanecb>cq;
+priority_queue<long long, vector<long long>, greater<long long>>bag;
+
+int N;
+int K;
+long long Ans;
+
+void input() {
+	cin >> N >> K;
+	for (int i = 0; i < N; i++)
+	{
+		long long m = 0;
+		long long v = 0;
+		cin >> m >> v;
+		Cv.push({ m,v });
+	}
+	for (int i = 0; i < K; i++)
+	{
+		long long Num = 0;
+		cin >> Num;
+		bag.push(Num);
+	}
+}
+
+int main() {
+
+	input();
+	// 가방 개수만큼 진행
+	for (int i = 0; i < K; i++)
+	{
+		long long nowbag = bag.top();
+		bag.pop();
+		while (!Cv.empty()) {
+			Cubic now = Cv.top();
+			if (now.weight <= nowbag) {
+				Cv.pop();
+				cq.push({ now.weight, now.value });
+			}
+			else {
+				break;
+			}
+		}
+		if (cq.size() != 0) {
+			lanecb fr = cq.top();
+			Ans += fr.value;
+			cq.pop();
+		}
+	}
+	cout << Ans;
+
+	return 0;
+}
